99c739c5ffaef4fe9c339b89649e800c8e2d5607
patbytes <patbytes256@gmail.com>
2021-11-19 09:26:14 -0600

    Migration to AsyncTaskLoader

--- app/src/main/java/edu/mssu/cis385/whowroteit/MainActivity.java
+++ app/src/main/java/edu/mssu/cis385/whowroteit/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.mssu.cis385.whowroteit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -11,11 +15,74 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
     private TextView mTitleText;
     private TextView mAuthorText;
 
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            int i = 0;
+            String title = null;
+            String authors = null;
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                i++;
+            }
+            if (title != null && authors != null) {
+                mTitleText.setText(title);
+                mAuthorText.setText(authors);
+            } else {
+                mTitleText.setText(R.string.no_results);
+                mAuthorText.setText("");
+            }
+
+        } catch (Exception e) {
+            // If onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
         mBookInput = (EditText)findViewById(R.id.bookInput);
         mTitleText = (TextView)findViewById(R.id.titleText);
         mAuthorText = (TextView)findViewById(R.id.authorText);
+
+        if(getSupportLoaderManager().getLoader(0)!=null){
+            getSupportLoaderManager().initLoader(0,null,this);
+        }
     }
 
     public void searchBooks(View view) {
@@ -45,7 +116,9 @@ public class MainActivity extends AppCompatActivity {
         }
         if (networkInfo != null && networkInfo.isConnected()
                 && queryString.length() != 0) {
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
             mAuthorText.setText("");
             mTitleText.setText(R.string.loading);
         } else {
